7536f98a90f94f38dab517833af951eb75ddb6c0
wise-wisdom <jjhww0703@gmail.com>
2023-04-11 17:41:03 +0900

    음식 추천 챗봇 완성

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import random
 from discord.ext import commands
 from configs import get_token
+import discord
 
 TOKEN = get_token()
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
-async def 밥(ctx):
+async def 오늘뭐먹지(ctx):
     food_options = [
-        "피자",
-        "햄버거",
-        "치킨",
-        "초밥",
-        "파스타"
+        "지지고",
+        "프랭크버거",
+        "한끼철판",
+        "푸른스시",
+        "후문식당"
     ]
-    
+
     random_choices = random.sample(food_options, 3)
     response = ', '.join(random_choices)
     await ctx.send(response)
